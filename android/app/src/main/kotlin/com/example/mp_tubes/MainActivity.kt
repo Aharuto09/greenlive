9f9be013053259fa81a846d63453f755e0044b38
Aharuto09 <56144971+Aharuto09@users.noreply.github.com>
2022-07-25 14:07:49 +0700

    Add files via upload

---
+++ android/app/src/main/kotlin/com/example/mp_tubes/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mp_tubes
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\membe\\Documents\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\membe\\Documents\\flutter project\\mp_tubes" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\membe\\Documents\\flutter"
+  "PROJECT_DIR=C:\\Users\\membe\\Documents\\flutter project\\mp_tubes"
+  "FLUTTER_ROOT=C:\\Users\\membe\\Documents\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\membe\\Documents\\flutter project\\mp_tubes\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\membe\\Documents\\flutter project\\mp_tubes"
+  "FLUTTER_TARGET=C:\\Users\\membe\\Documents\\flutter project\\mp_tubes\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\membe\\Documents\\flutter project\\mp_tubes\\.dart_tool\\package_config.json"
+)
